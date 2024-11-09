e23da591e34c5c2b0f1de2fe4012b815816e9dd4
nyxnmigi5 <a64569773@gmail.com>
2024-11-09 16:28:47 +0900

    S2 add

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,6 @@
 
 export default function Home() {
   return (
-    <div>hello, 2024-11-09 <b>가나다</b> <br>석원쌤 S2</br></div>
+    <div><p>hello, 2024-11-09 <b>가나다</b></p><p> 석원쌤 S2</p></div>
   );
 }
