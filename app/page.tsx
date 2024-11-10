dc19b84279b37b134712ab84407d07d8b7df74ce
nyxnmigi5 <a64569773@gmail.com>
2024-11-10 10:19:29 +0900

    button_style

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
 
 
 export default function Home() {
+
+  var styleObj = {
+    "background": "white"
+  }
+
   return (
-    <div><p>hello, 2024-11-09 <b>가나다</b> <br/><b>석원쌤 S2</b></p></div>
+    <div>
+      <p>
+        hello, 2024-11-09 <b>가나다</b><br/>
+        <b>석원쌤 S2</b>
+      </p>
+      <button type = "button" style = {styleObj}>버튼</button>
+    </div>
   );
 }
