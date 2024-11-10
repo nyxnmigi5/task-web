e731f25ccfa7e048fb7e3bc424b9326a2826b6a7
nyxnmigi5 <a64569773@gmail.com>
2024-11-10 13:51:02 +0900

    button_name

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,27 @@ export default function Home() {
   return (
     <div>
       {/* {HTML 태그처럼 쓸 수 있음} */}
+      <MyButton name="jimin"/>
+      {/* <MyButton/>
       <MyButton/>
+      <MyButton/> */}
     </div>
   );
 }
 
 // <button>I am button</button>을 생성하는 JS 함수
-function MyButton(){
-  return<button>I am a button!!</button>
+// function MyButton({
+//   name
+// }:{
+//   name: string
+// }){
+//   return<button>I am {name}</button>
+// }
+
+type TParams = {
+  name: string
+}
+
+function MyButton(params: TParams){
+  return<button>I am {params.name}</button>
 }
